9e0d7fa778475c9f5a4d8517c94ba656a0688e40
PuBHARGAVI <saibhargavi.puvvada@gmail.com>
2021-02-17 23:32:31 +0530

    1499215commit3

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 // Functional component
-const Container=()=>{
+/*const Container=()=>{
     return React.createElement('div',null,"Hey ProGrads! Welcome to React Learning",
     React.createElement('div',null,`Let's rock and roll`));
 }
 const container=document.getElementById("react-container");
 //rendering the container
 ReactDOM.render(React.createElement(Container),container);
-
+*/
+class ReactContainer extends React.Component{
+    render(){
+        return React.createElement('div',null,'Hey ProGrads',
+        React.createElement('div',null,"Let's rock and roll"));
+    }
+}
+const container=document.getElementById("react-container");
+ReactDOM.render(React.createElement(ReactContainer),container);
 
